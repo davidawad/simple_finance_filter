b68be5be02c27ec2cdaf9b2defc11843521e556d
David Awad <Davidawad64@gmail.com>
2018-03-19 17:57:46 -0400

    Update formatter.py

--- formatter.py
+++ formatter.py
@@ -4,18 +4,17 @@ import csv
 
 f=pd.read_csv("input.csv")
 
+# these are the columns that the native simple CSV export uses
 total_columns = ['Date', 'Recorded at', 'Scheduled for', 'Amount', 'Activity', 'Pending', 'Raw description']
 
+# these are the columns I use in my spreadsheet
 filtered_columns = ['Date', 'Amount', 'Description', 'Category']
 
-# we only want the negative numbers, we only care about the money going outwards
+# for me use case I only want the negative numbers. 
+# I only want to know about money I'm spending. 
 f = f[filtered_columns].query('Amount < 0')
 
-# take the money we've spent and convert it to positive money
+# take the money we've spent and convert it to positive integers
 f['Amount'] = f['Amount'].abs()
 
-#  import pdb; pdb.set_trace()
-
-#  new_f = f[keep_col]
 f.to_csv("output.csv", index=False)
-
