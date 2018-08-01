38c04007a96b43ae7cbdc45e9d8b160b316fbe21
david awad <me@davidawad.com>
2018-08-01 16:35:26 -0400

    stupidity of not taking command line input

--- formatter.py
+++ formatter.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import csv
+from sys import argv
 
 
-f=pd.read_csv("input.csv")
+f=pd.read_csv(argv[1])
 
 # these are the columns that the native simple CSV export uses
 total_columns = ['Date', 'Recorded at', 'Scheduled for', 'Amount', 'Activity', 'Pending', 'Raw description']
@@ -10,8 +11,8 @@ total_columns = ['Date', 'Recorded at', 'Scheduled for', 'Amount', 'Activity', '
 # these are the columns I use in my spreadsheet
 filtered_columns = ['Date', 'Amount', 'Description', 'Category']
 
-# for me use case I only want the negative numbers. 
-# I only want to know about money I'm spending. 
+# for me use case I only want the negative numbers.
+# I only want to know about money I'm spending.
 f = f[filtered_columns].query('Amount < 0')
 
 # take the money we've spent and convert it to positive integers
